dcb5b9f40eb3d3f0ab4fda1de6288a29f19e4b14
David Paul <dpaul@eyenalyze.com>
2019-04-01 15:57:51 -0500

    Updating unit test

--- tests/Service/GamePlayServiceTest.php
+++ tests/Service/GamePlayServiceTest.php
@@ -4,13 +4,13 @@ namespace Tests\AppBundle\Service;
 
 use App\Document\Game;
 use App\Service\GamePlayService;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use PHPUnit\Framework\TestCase;
 
 class GamePlayServiceTest extends TestCase {
 
     public function testRandomPlay() {
-        $em = $this->getMockBuilder(EntityManager::class)
+        $em = $this->getMockBuilder(DocumentManager::class)
             ->disableOriginalConstructor()
             ->getMock();
         $gps = new GamePlayService($em);
